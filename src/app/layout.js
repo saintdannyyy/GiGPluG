6d1cb9414f9ad4a78cb9f103a9368e2480df7f49
Saintdannyyy <danieltesla746@gmail.com>
2025-03-26 22:05:48 +0000

    Fix import path for Vercel Analytics in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { Analytics } from "../../node_modules/@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "GIGPLUG",
